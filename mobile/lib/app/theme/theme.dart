cd04ed7249a56fc12e56a178a1d48be294f709d5
0thigs <machinimavlogs@gmail.com>
2026-04-26 22:49:48 -0300

    ♻️refactor: G5-106 update app theme

--- mobile/lib/app/theme/theme.dart
+++ mobile/lib/app/theme/theme.dart
@@ -4,20 +4,20 @@ import '../../shared/constants/app_text_styles.dart';
 
 final class AppTheme {
   static ThemeData get theme => ThemeData(
-        useMaterial3: true,
-        primaryColor: AppColors.primary,
-        scaffoldBackgroundColor: AppColors.background,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: AppColors.primary,
-          primary: AppColors.primary,
-          surface: AppColors.surface,
-          error: AppColors.error,
-        ),
-        textTheme: const TextTheme(
-          displayLarge: AppTextStyles.h1,
-          titleLarge: AppTextStyles.h2,
-          bodyLarge: AppTextStyles.body,
-          bodySmall: AppTextStyles.caption,
-        ),
-      );
+    useMaterial3: true,
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.background,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: AppColors.primary,
+      primary: AppColors.primary,
+      surface: AppColors.surface,
+      error: AppColors.error,
+    ),
+    textTheme: const TextTheme(
+      displayLarge: AppTextStyles.h1,
+      titleLarge: AppTextStyles.h2,
+      bodyLarge: AppTextStyles.body,
+      bodySmall: AppTextStyles.caption,
+    ),
+  );
 }
