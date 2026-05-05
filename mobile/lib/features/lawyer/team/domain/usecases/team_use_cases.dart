3f4e93b1c70df016461af4483900ead1c4b3ecc0
0thigs <machinimavlogs@gmail.com>
2026-05-05 13:31:57 -0300

    feat(mobile): contrato e use cases da equipe
    TeamRepository + 5 use cases (List/Get/Add/UpdatePermissions/Remove)
    seguindo o padrao Either<Failure, T> ja consolidado nas demais features.

---
+++ mobile/lib/features/lawyer/team/domain/usecases/team_use_cases.dart
@@ -0,0 +1,48 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:mobile/shared/errors/failures.dart';
+
+import '../entities/team_member.dart';
+import '../entities/team_member_created.dart';
+import '../entities/team_member_draft.dart';
+import '../repositories/team_repository.dart';
+
+final class ListTeamUseCase {
+  final TeamRepository _repository;
+  const ListTeamUseCase(this._repository);
+
+  Future<Either<Failure, List<TeamMember>>> call() => _repository.listTeam();
+}
+
+final class GetTeamMemberUseCase {
+  final TeamRepository _repository;
+  const GetTeamMemberUseCase(this._repository);
+
+  Future<Either<Failure, TeamMember>> call(String id) =>
+      _repository.getMember(id);
+}
+
+final class AddTeamMemberUseCase {
+  final TeamRepository _repository;
+  const AddTeamMemberUseCase(this._repository);
+
+  Future<Either<Failure, TeamMemberCreated>> call(TeamMemberDraft draft) =>
+      _repository.addMember(draft);
+}
+
+final class UpdateTeamMemberPermissionsUseCase {
+  final TeamRepository _repository;
+  const UpdateTeamMemberPermissionsUseCase(this._repository);
+
+  Future<Either<Failure, TeamMember>> call(
+    String id,
+    Map<String, bool> permissions,
+  ) => _repository.updatePermissions(id, permissions);
+}
+
+final class RemoveTeamMemberUseCase {
+  final TeamRepository _repository;
+  const RemoveTeamMemberUseCase(this._repository);
+
+  Future<Either<Failure, Unit>> call(String id) =>
+      _repository.removeMember(id);
+}
